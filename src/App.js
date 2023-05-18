8b75f5c1faca34ab45866af7c7e8d348c520f6be
Alexandr_Ovchar <azalexxx02@gmail.com>
2023-05-18 18:29:01 +0300

    reduxToolkit

--- src/App.js
+++ src/App.js
@@ -12,25 +12,44 @@ import Skeleton from "./components/pizzaBlock/Skeleton";
 import "./scss/app.scss";
 import { Route, Routes } from "react-router-dom";
 
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment } from './redux/slices/filterSlice'
+
 export const SearchContext = React.createContext();
 // console.log('search', SearchContext)
 
 function App() {
+  const count = useSelector((state) => state.counter.count);
+
+  const dispatch = useDispatch();
   const [searchValue, setSearchValue] = React.useState("");
   console.log("app", searchValue);
 
   return (
     <div className="wrapper">
-      <SearchContext.Provider value={{searchValue, setSearchValue}}>
+      <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+      {/* <SearchContext.Provider value={{ searchValue, setSearchValue }}>
         <Header />
         <div className="content">
           <Routes>
-            <Route path="/" element={<Home  />} />
+            <Route path="/" element={<Home />} />
             <Route path="/cart" element={<Cart />} />
             <Route path="*" element={<NotFaund />} />
           </Routes>
         </div>
-      </SearchContext.Provider>
+      </SearchContext.Provider> */}
     </div>
   );
 }
